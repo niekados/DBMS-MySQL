49745620a44ba1a64561d0ded41101f773d1ca0a
niekados <tiny.post@yahoo.com>
2021-09-05 23:58:22 +0000

    wefrwfwfvwvewf

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,5 +1,6 @@
 
 import os
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -14,11 +15,15 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
+# connection.cursor() will produse duples, so to get Dictionary, we need to add argument- pymysql.cursors.DictCursor 
+
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        names = ["Zoe", "Tom"]
+        cursor.execute("delete from Friends where Name in ('Jim', 'Bob')")
+
+        
+        connection.commit()
+
 finally:
     # Close the connection, regardless of whether or not the above was successful
     connection.close()
